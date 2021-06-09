fix sph2erp and erp2sph error; fix of end point rotation

code/python/test/test_projection.py
@@ -22,7 +22,7 @@ def test_get_rotation(erp_src_image_path, erp_flow_path):
     erp_flow = flow_post_proc.of_nonerp2erp(erp_flow)
 
     # get the rotation
-    erp_image_rotated = projection.image_align(erp_image, erp_flow)
+    erp_image_rotated = projection.image_rotate_flow(erp_image, erp_flow)
 
     # rotate the src image
     rotated_image_path = erp_src_image_path + "_rotated.jpg"
@@ -31,32 +31,36 @@ def test_get_rotation(erp_src_image_path, erp_flow_path):
 
 
 def test_flow_accumulate_endpoint(erp_src_image_filepath, erp_tar_image_filepath):
-    """ Always rotate the target image.
-
-    TODO if set rotation_theta and rotation_phi to 0.0, there are offset between the input and output image. Check the reason! 
+    """ 
+    Test warp optical flow. Always rotate the target image.
     """
     src_image_data = image_io.image_read(erp_src_image_filepath)
     tar_image_data = image_io.image_read(erp_tar_image_filepath)
 
     # 0) rotation tar image
-    # Note: 
     rotation_theta = np.radians(10.0)
     rotation_phi = np.radians(10.0)
-    tar_image_data_rot = spherical_coordinates.rotate_array(tar_image_data, rotation_theta, rotation_phi)
-    image_io.image_save(tar_image_data_rot, erp_tar_image_filepath + "_rot.jpg") 
 
     # 1) compute the flow from src to rotated rotated tar image
-    flow_dis = flow_estimate.DIS(src_image_data, tar_image_data_rot)
-    flow_vis_data = flow_vis.flow_to_color(flow_dis)
+    tar_image_data_rot = spherical_coordinates.rotate_erp_array(src_image_data, rotation_theta, rotation_phi)
+    image_io.image_save(tar_image_data_rot, erp_tar_image_filepath + "_rot.jpg")
+    # # genera
+    # flow_dis = flow_estimate.of_methdod_DIS(src_image_data, tar_image_data_rot)
+    flow_dis = spherical_coordinates.rotation2erp_motion_vector(src_image_data.shape[0:2], rotation_theta, rotation_phi)
+    flow_vis_data = flow_vis.flow_to_color(flow_dis, min_ratio=0.2, max_ratio=0.8)
     image_io.image_save(flow_vis_data, erp_src_image_filepath + "_flow.jpg")
+    tar_image_data_warp = flow_warp.warp_backward(tar_image_data_rot, flow_dis)
+    image_io.image_save(tar_image_data_warp, erp_tar_image_filepath + "_rot_flow.jpg")
 
-    # 2) get the flow from src to original tar image
+    # 2) get the flow from src to tar image
     # warp the optical flow base on rotation
-    flow_dis_rot = projection.flow_accumulate_endpoint(flow_dis, [-rotation_theta, -rotation_phi])
-    flow_vis_data = flow_vis.flow_to_color(flow_dis_rot)
+    flow_dis_rot = projection.flow_rotate_endpoint(flow_dis, [-rotation_theta, -rotation_phi])
+    flow_vis_data = flow_vis.flow_to_color(flow_dis_rot, min_ratio=0.2, max_ratio=0.8)
     image_io.image_save(flow_vis_data, erp_src_image_filepath + "_flow_rot.jpg")
-    src_image_data_rot = flow_warp.warp_forward(src_image_data, flow_dis_rot)
-    image_io.image_save(src_image_data_rot, erp_src_image_filepath + "_warp_rot.jpg")
+    src_image_data_rot = flow_warp.warp_forward(src_image_data, flow_dis_rot, True)
+    image_io.image_save(src_image_data_rot, erp_src_image_filepath + "_warp_forward_rot.jpg")
+    src_image_data_rot = flow_warp.warp_backward(src_image_data, flow_dis_rot)
+    image_io.image_save(src_image_data_rot, erp_src_image_filepath + "_warp_backward_rot.jpg")
 
 
 if __name__ == "__main__":

code/python/test/test_spherical_coordinates.py
@@ -68,27 +68,67 @@ def test_great_circle_distance_uv():
             log.error(" ")
 
 
+def test_rotation2erp_motion_vector(erp_src_image_filepath):
+    src_image_data = image_io.image_read(erp_src_image_filepath)
+    image_size = src_image_data.shape[0:2]
+    rotation_theta = np.radians(0.0)
+    rotation_phi = np.radians(0.0)
+    erp_motion_vector = sc.rotation2erp_motion_vector(image_size, rotation_theta, rotation_phi)
+    # flow_color = flow_vis.flow_to_color(erp_motion_vector)
+    # image_io.image_save(flow_color,erp_src_image_filepath + "_flow.jpg" )
+    flow_color = flow_vis.flow_value_to_color(erp_motion_vector)
+
+
+def test_rotate_erp_array(erp_src_image_filepath):
+    src_image_data = image_io.image_read(erp_src_image_filepath)
+
+    # double side rotation
+    rotation_theta = np.radians(10.0)
+    rotation_phi = np.radians(30.0)
+    tar_image_data_rot, rotation_mat = sc.rotate_erp_array(src_image_data, rotation_theta, rotation_phi)
+    image_io.image_save(tar_image_data_rot, erp_src_image_filepath + "_rot_0.jpg")
+    tar_image_data_rot, _ = sc.rotate_erp_array(tar_image_data_rot, rotation_mat=rotation_mat.T)
+    image_io.image_save(tar_image_data_rot, erp_src_image_filepath + "_rot_1.jpg")
+
+    # multi wrap around
+    rotation_theta = np.radians(90.0)
+    rotation_phi = np.radians(0.0)
+    rotation_theta = np.radians(0.0)
+    rotation_phi = np.radians(45.0)
+    tar_image_data_rot = src_image_data
+    for index in range(0, 32):
+        tar_image_data_rot = sc.rotate_erp_array(tar_image_data_rot, rotation_theta, rotation_phi)
+    image_io.image_save(tar_image_data_rot, erp_src_image_filepath + "_rot_2.jpg")
+
+
 def test_rotate_array_coord(erp_src_image_filepath):
     src_image_data = image_io.image_read(erp_src_image_filepath)
-    image_size = [960, 480]
+    image_size = src_image_data.shape[0:2]
     rotation_theta = np.radians(0.0)
     rotation_phi = np.radians(30.0)
 
-    # # envmap rotate
-    # tar_image_data_rot = sc.rotate_array(src_image_data, rotation_theta, rotation_phi)
-    # image_io.image_save(tar_image_data_rot, erp_src_image_filepath + "_rot.jpg")
+    #  rotate
+    tar_image_data_rot = sc.rotate_erp_array(src_image_data, rotation_theta, rotation_phi)
+    image_io.image_save(tar_image_data_rot, erp_src_image_filepath + "_rot.jpg")
 
     # self rotate
-    erp_motion_vector = sc.rotate_erp_motion_vector(image_size, -rotation_theta, -rotation_phi)
-    erp_motion_vector = np.moveaxis(erp_motion_vector, 0, -1)
-    src_image_data_rot = flow_warp.warp_forward(src_image_data, erp_motion_vector)
+    erp_motion_vector = sc.rotation2erp_motion_vector(image_size, rotation_theta, rotation_phi)
+    # erp_motion_vector = np.moveaxis(erp_motion_vector, 0, -1)
+
     erp_motion_vector_vis = flow_vis.flow_to_color(erp_motion_vector)
     image_io.image_save(erp_motion_vector_vis, erp_src_image_filepath + "_flow_vis.jpg")
-    image_io.image_save(src_image_data_rot, erp_src_image_filepath + "_warp_rot.jpg")
+
+    # src_image_data_rot = flow_warp.warp_forward(tar_image_data_rot, erp_motion_vector)
+    # image_io.image_save(src_image_data_rot, erp_src_image_filepath + "_warp_forward_rot.jpg")
+
+    tar_image_data_rot = flow_warp.warp_backward(tar_image_data_rot, erp_motion_vector)
+    image_io.image_save(tar_image_data_rot, erp_src_image_filepath + "_warp_backward_rot.jpg")
 
 
 if __name__ == "__main__":
     # test_great_circle_distance_uv()
 
     erp_src_image_filepath = os.path.join(config.TEST_data_root_dir, "replica_360/apartment_0/0001_rgb.jpg")
-    test_rotate_array_coord(erp_src_image_filepath)
+    # test_rotate_array_coord(erp_src_image_filepath)
+    test_rotate_erp_array(erp_src_image_filepath)
+    # test_rotation2erp_motion_vector(erp_src_image_filepath)

code/python/utility/flow_vis.py
@@ -20,7 +20,6 @@ https://github.com/tomrunia/OpticalFlow_Visualization
 
 def make_colorwheel():
     """
-    # TODO change comment style
     Generates a color wheel for optical flow visualization as presented in:
         Baker et al. "A Database and Evaluation Methodology for Optical Flow" (ICCV, 2007)
         URL: http://vision.middlebury.edu/flow/flowEval-iccv07.pdf
@@ -28,10 +27,9 @@ def make_colorwheel():
     Code follows the original C++ source code of Daniel Scharstein.
     Code follows the the Matlab source code of Deqing Sun.
 
-    Returns:
-        np.ndarray: Color wheel
+    :return: Color wheel
+    :rtype: numpy
     """
-
     RY = 15
     YG = 6
     GC = 4
@@ -71,19 +69,19 @@ def make_colorwheel():
 
 def flow_uv_to_colors(u, v, convert_to_bgr=False):
     """
-    # TODO change comment style
     Applies the flow color wheel to (possibly clipped) flow components u and v.
 
     According to the C++ source code of Daniel Scharstein
     According to the Matlab source code of Deqing Sun
 
-    Args:
-        u (np.ndarray): Input horizontal flow of shape [H,W]
-        v (np.ndarray): Input vertical flow of shape [H,W]
-        convert_to_bgr (bool, optional): Convert output image to BGR. Defaults to False.
-
-    Returns:
-        np.ndarray: Flow visualization image of shape [H,W,3]
+    :param u:  Input horizontal flow of shape [H,W]
+    :type u: numpy
+    :param v: Input vertical flow of shape [H,W]
+    :type v: numpy
+    :param convert_to_bgr: Convert output image to BGR. Defaults to False.
+    :type convert_to_bgr: bool, optional
+    :return:  Flow visualization image of shape [H,W,3]
+    :rtype: numpy
     """
     flow_image = np.zeros((u.shape[0], u.shape[1], 3), np.uint8)
     colorwheel = make_colorwheel()  # shape [55x3]
@@ -122,7 +120,7 @@ def flow_to_color(flow_uv, clip_flow=None, convert_to_bgr=False, min_ratio=0.0, 
     :rtype: numpy
     """
     # get the clip range
-    if min_ratio !=0 and max_ratio!=1.0:
+    if min_ratio != 0 and max_ratio != 1.0:
         clip_flow = image_evaluate.get_min_max(flow_uv, min_ratio, max_ratio)
 
     # u_min,u_max = image_evaluate.get_min_max(flow_uv[:,:,0], min_ratio, max_ratio)
@@ -141,10 +139,11 @@ def flow_to_color(flow_uv, clip_flow=None, convert_to_bgr=False, min_ratio=0.0, 
     epsilon = 1e-5
     u = u / (rad_max + epsilon)
     v = v / (rad_max + epsilon)
+    # TODO add color wheel and range to image.
     return flow_uv_to_colors(u, v, convert_to_bgr)
 
 
-def flow_value_to_color(flow_uv, output_path=None, min_ratio=0.0, max_ratio=1.0, visual_colormap = "RdPu"):
+def flow_value_to_color(flow_uv, output_path=None, min_ratio=0.0, max_ratio=1.0, visual_colormap="RdPu"):
     """ Visualize U,V and show the bar.
 
     :param flow_uv: optical flow. [height, width, 2]
@@ -189,7 +188,6 @@ def flow_max_min_visual(flow_data, output_path=None,  min_ratio=0.1, max_ratio=0
     # new_u = np.where(flow_data[:,:,0] > vmax_, flow_data[:,:,0], vmin_)
     from skimage.morphology import dilation, square
 
-
     def image_process(flow_data):
         dilation_square_size = 10
         return dilation(flow_data, square(dilation_square_size))
@@ -203,28 +201,28 @@ def flow_max_min_visual(flow_data, output_path=None,  min_ratio=0.1, max_ratio=0
     flow_data_u_min = np.where(flow_data[:, :, 0] < u_ratio_vmin_, -flow_data[:, :, 0] + u_ratio_vmin_, 0)
     # im = axes[0, 0].imshow(flow_data_u_min, cmap=plt.get_cmap("Greys") )#, vmin=0, vmax=u_ratio_vmin_)
     im = axes[0, 0].imshow(image_process(flow_data_u_min), cmap=plt.get_cmap("Greys"))  # , vmin=0, vmax=u_ratio_vmin_)
-    figure.colorbar(im, ax=axes[0, 0])#.ravel().tolist())
+    figure.colorbar(im, ax=axes[0, 0])  # .ravel().tolist())
     # u max
     axes[0, 1].get_xaxis().set_visible(False)
     axes[0, 1].get_yaxis().set_visible(False)
     axes[0, 1].set_title("Optical Flow (U) Max")
     flow_data_u_max = np.where(flow_data[:, :, 0] > u_ratio_vmax_, flow_data[:, :, 0] - u_ratio_vmax_, 0)
     im = axes[0, 1].imshow(image_process(flow_data_u_max), cmap=plt.get_cmap("Greys"))  # , vmin=u_ratio_vmax_, vmax=u_vmax_)
-    figure.colorbar(im, ax=axes[0, 1])#.ravel().tolist())
+    figure.colorbar(im, ax=axes[0, 1])  # .ravel().tolist())
     # v min
     axes[1, 0].get_xaxis().set_visible(False)
     axes[1, 0].get_yaxis().set_visible(False)
     axes[1, 0].set_title("Optical Flow (V) Min")
     flow_data_v_min = np.where(flow_data[:, :, 1] < v_ratio_vmin_, -flow_data[:, :, 1] + v_ratio_vmin_, 0)
     im = axes[1, 0].imshow(image_process(flow_data_v_min), cmap=plt.get_cmap("Greys"))  # , vmin=v_vmin_, vmax=v_ratio_vmin_)
-    figure.colorbar(im, ax=axes[1, 0])#.ravel().tolist())
+    figure.colorbar(im, ax=axes[1, 0])  # .ravel().tolist())
     # v max
     axes[1, 1].get_xaxis().set_visible(False)
     axes[1, 1].get_yaxis().set_visible(False)
     axes[1, 1].set_title("Optical Flow (V) Max")
     flow_data_v_max = np.where(flow_data[:, :, 1] > v_ratio_vmax_, flow_data[:, :, 1] - v_ratio_vmax_, 0)
     im = axes[1, 1].imshow(image_process(flow_data_v_max), cmap=plt.get_cmap("Greys"))  # , vmin=v_ratio_vmax_, vmax=v_vmax_)
-    figure.colorbar(im, ax=axes[1, 1])#.ravel().tolist())
+    figure.colorbar(im, ax=axes[1, 1])  # .ravel().tolist())
 
     # figure.tight_layout()
     if output_path is not None:

code/python/utility/flow_warp.py
@@ -1,28 +1,48 @@
 import numpy as np
 from scipy import ndimage
 
+from logger import Logger
+
+log = Logger(__name__)
+log.logger.propagate = False
+
+
 def warp_backward(image_target, of_forward):
     """
-    forward warp with optical flow. 
-    warp image with interpolation, scipy.ndimage.map_coordinates
-    """
-    image_size = np.shape(image_target)
-    image_height = image_size[0]
-    image_width = image_size[1]
-    image_channels = image_size[2]
+    Backward warp with optical flow from the target image to generate the source image. 
 
-    dest_image = np.zeros(np.shape(image_target), dtype=image_target.dtype)
+    :param image_target: The terget image of optical flow, [height, width, channel].
+    :type image_target: numpy
+    :param of_forward:  optical flow from source to target, [height, width, 2].
+    :type of_forward: numpy
+    :return: Generated source image.
+    :rtype: numpy
+    """
+    image_height = image_target.shape[0]
+    image_width = image_target.shape[1]
+    image_channels = None
+    if len(image_target.shape) == 3:
+        image_channels = image_target.shape[2]
+    elif len(image_target.shape) == 2:
+        image_channels = None
+    else:
+        log.error("The image shape is {}, do not support.".format(image_target.shape))
+    dest_image = np.zeros_like(image_target, dtype=image_target.dtype)
 
     # 0) comput new location
     x_idx_arr = np.linspace(0, image_width - 1, image_width)
     y_idx_arr = np.linspace(0, image_height - 1, image_height)
-
     x_idx, y_idx = np.meshgrid(x_idx_arr, y_idx_arr)
+    # x_idx_new = np.remainder(x_idx + of_forward[:, :, 0], image_width)
+    # y_idx_new = np.remainder(y_idx + of_forward[:, :, 1], image_height)
     x_idx_new = (x_idx + of_forward[:, :, 0])
     y_idx_new = (y_idx + of_forward[:, :, 1])
 
-    for channel_index in range(0, image_channels):
-        dest_image[y_idx.astype(int), x_idx.astype(int), channel_index] = ndimage.map_coordinates(image_target[:, :, channel_index], [y_idx_new, x_idx_new], order=1, mode='constant', cval=255)
+    if image_channels is not None:
+        for channel_index in range(0, image_channels):
+            dest_image[y_idx.astype(int), x_idx.astype(int), channel_index] = ndimage.map_coordinates(image_target[:, :, channel_index], [y_idx_new, x_idx_new], order=1, mode='wrap')
+    else:
+        dest_image[y_idx.astype(int), x_idx.astype(int)] = ndimage.map_coordinates(image_target[:, :], [y_idx_new, x_idx_new], order=1, mode='constant', cval=255)
 
     return dest_image
 
@@ -75,13 +95,13 @@ def warp_forward_padding(image_target, of_forward, padding_x=0, padding_y=0):
     return image_src
 
 
-def warp_forward(image_first, of_forward, wrap_around = False, ignore_transparent = False):
+def warp_forward(image_first, of_forward, wrap_around=False, ignore_transparent=False):
     """ forward warp with optical flow. 
     warp image with interpolation, scipy.ndimage.map_coordinates
 
-    :param image_first: input image, when it's 4 channels image, use the alpha channel to ignore the transparent area.
+    :param image_first: input image, when it's 4 channels image, use the alpha channel to ignore the transparent area [height,width,:].
     :type image_first: numpy
-    :param of_forward: forward optical flow. [width, height, 2]
+    :param of_forward: forward optical flow. [height, width,  2]
     :type of_forward: numpy
     :param wrap_around: whether process the wrap around pixels, defaults to False
     :type wrap_around: bool, optional
@@ -91,46 +111,44 @@ def warp_forward(image_first, of_forward, wrap_around = False, ignore_transparen
     :rtype: numpy
     """
     valid_pixels_index = None
-    if image_first.shape[2] ==4:
+    if image_first.shape[2] == 4:
         # RGBA images, ignore the transparent area
-        valid_pixels_index = image_first[:,:,3] == 255
+        valid_pixels_index = image_first[:, :, 3] == 255
 
     image_size = np.shape(image_first)
     image_height = image_size[0]
     image_width = image_size[1]
     image_channels = image_size[2]
 
-    dest_image = np.zeros(np.shape(image_first), dtype=image_first.dtype)
-
     # 0) comput new location
     x_idx_arr = np.linspace(0, image_width - 1, image_width)
     y_idx_arr = np.linspace(0, image_height - 1, image_height)
-
     x_idx, y_idx = np.meshgrid(x_idx_arr, y_idx_arr)
-    x_idx_new = (x_idx + of_forward[:, :, 0] + 0.5).astype(int)
-    y_idx_new = (y_idx + of_forward[:, :, 1] + 0.5).astype(int)
+    # x_idx, y_idx = np.mgrid[0:image_height, 0: image_width]
+    x_idx_new = x_idx + of_forward[:, :, 0]
+    y_idx_new = y_idx + of_forward[:, :, 1]
 
     # check index out of the image bounds
-    if not wrap_around:
-        x_idx_new = np.where(x_idx_new > 0, x_idx_new, 0)
-        x_idx_new = np.where(x_idx_new < image_width - 1, x_idx_new, image_width - 1)
-
-        y_idx_new = np.where(y_idx_new > 0, y_idx_new, 0)
-        y_idx_new = np.where(y_idx_new < image_height - 1, y_idx_new, image_height - 1)
-    else:
-        # TODO 
+    if wrap_around:
         x_idx_new = np.where(x_idx_new > 0, x_idx_new, x_idx_new + image_width)
-        x_idx_new = np.where(x_idx_new < image_width - 1, x_idx_new, np.remainder(x_idx_new, image_width))
-
+        x_idx_new = np.where(x_idx_new < image_width, x_idx_new, np.remainder(x_idx_new, image_width))
         y_idx_new = np.where(y_idx_new > 0, y_idx_new, y_idx_new + image_height)
-        y_idx_new = np.where(y_idx_new < image_height - 1, y_idx_new, np.remainder(y_idx_new, image_height))
+        y_idx_new = np.where(y_idx_new < image_height, y_idx_new, np.remainder(y_idx_new, image_height))
+    else:
+        x_idx_new = np.where(x_idx_new > 0, x_idx_new, 0)
+        x_idx_new = np.where(x_idx_new < image_width, x_idx_new, image_width - 1)
+        y_idx_new = np.where(y_idx_new > 0, y_idx_new, 0)
+        y_idx_new = np.where(y_idx_new < image_height, y_idx_new, image_height - 1)
 
-    if not valid_pixels_index is None:
+    if valid_pixels_index is not None:
         x_idx_new = x_idx_new[valid_pixels_index]
         y_idx_new = y_idx_new[valid_pixels_index]
         x_idx = x_idx[valid_pixels_index]
         y_idx = y_idx[valid_pixels_index]
 
+    x_idx_new = x_idx_new.astype(int)
+    y_idx_new = y_idx_new.astype(int)
+    dest_image = np.zeros(np.shape(image_first), dtype=image_first.dtype)
     for channel_index in range(0, image_channels):
         dest_image[y_idx_new, x_idx_new, channel_index] = ndimage.map_coordinates(image_first[:, :, channel_index], [y_idx, x_idx], order=1, mode='constant', cval=255)
 

code/python/utility/projection.py
@@ -1,12 +1,11 @@
 import numpy as np
 from scipy import ndimage
-
-import gnomonic_projection
 from scipy.stats import norm
 
-from logger import Logger
-import spherical_coordinates
+import gnomonic_projection
+import spherical_coordinates as sc
 
+from logger import Logger
 log = Logger(__name__)
 log.logger.propagate = False
 
@@ -167,7 +166,7 @@ def get_blend_weight_cubemap(face_x_src_gnomonic, face_y_src_gnomonic,
     return weight_map
 
 
-def flow2offset(erp_flow, use_weight=True):
+def flow2sph_rotation(erp_flow, use_weight=True):
     """Compute the  two image rotation from the ERP image's optical flow.
     The rotation is from the first image to second image.
 
@@ -204,56 +203,56 @@ def flow2offset(erp_flow, use_weight=True):
     return theta_delta, phi_delta
 
 
-def image_align(erp_image, erp_flow):
-    """Backward rotate the target image base on the flow. 
-    Make it align withe the source ERP image.
+def image_rotate_flow(erp_image, erp_flow):
+    """
+    Rotate the ERP image base on the flow. 
+    The flow is from erp_image to another image.
 
-    :param erp_image_src: the flow's target image
-    :type erp_image_src: numpy 
-    :param erp_flow: the erp image's flow, the optical flow is not processed the wrap around.
+    :param erp_image: the flow's ERP image, the image is 
+    :type erp_image: numpy 
+    :param erp_flow: the erp image's flow.
     :type erp_flow: numpy 
-    :return: The rotated target image
+    :return: The rotated ERP image
     :rtype: numpy
     """
-    # 0) compuate the average of optical flow & get the delta theta and phi
-    theta_delta, phi_delta = flow2offset(erp_flow)
-
-    from scipy.spatial.transform import Rotation as R
-    rotation_matrix = R.from_euler("xyz", [np.degrees(phi_delta), np.degrees(theta_delta), 0], degrees=True).as_dcm()
-
-    # 1) rotate the ERP image
-    from envmap import EnvironmentMap
-    envmap = EnvironmentMap(erp_image, format_='latlong')
-    erp_image_rot = envmap.rotate("DCM", rotation_matrix).data
-
+    # compuate the average of optical flow & get the delta theta and phi
+    # TODO check the flow overflow or not
+    theta_delta, phi_delta = flow2sph_rotation(erp_flow)
+    # rotate the ERP image
+    erp_image_rot = sc.rotate_erp_array(erp_image, theta_delta, phi_delta)
     if erp_image.dtype == np.uint8:
         erp_image_rot = erp_image_rot.astype(np.uint8)
+    return erp_image_rot, theta_delta, phi_delta
 
-    return erp_image_rot, [theta_delta, phi_delta]
 
-
-def flow_accumulate_endpoint(optical_flow, rotation):
+def flow_rotate_endpoint(optical_flow, rotation):
     """ Add the rotation offset to the end points of optical flow.
-    And return the new optical flow.
 
-    :param optical_flow: the original optical flow
+    :param optical_flow: the original optical flow, [height, width, 2]
     :type optical_flow: numpy
     :param rotation: the rotation of spherical coordinate in radian, [theta, phi]
-    :type rotation: list
-    :return: the accumulated optical flow
+    :type rotation: tuple
+    :return: the new optical flow
     :rtype: numpy 
     """
     flow_height = optical_flow.shape[0]
     flow_width = optical_flow.shape[1]
-
     end_points_array_x = np.linspace(0, flow_width, flow_width, endpoint=False)
     end_points_array_y = np.linspace(0, flow_height, flow_height, endpoint=False)
     src_points_array_xv, src_points_array_yv = np.meshgrid(end_points_array_x, end_points_array_y)
 
     # get end point location in ERP coordinate
-    end_points_array_xv = src_points_array_xv + optical_flow[:, :, 0]
-    end_points_array_yv = src_points_array_yv + optical_flow[:, :, 1]
+    end_points_array_xv = np.remainder(src_points_array_xv + optical_flow[:, :, 0], flow_width)
+    end_points_array_yv = np.remainder(src_points_array_yv + optical_flow[:, :, 1], flow_height)
+    end_points_array = sc.rotation2erp_motion_vector((flow_height, flow_width),  rotation[0],  rotation[1])
 
+    end_points_array_xv += ndimage.map_coordinates(end_points_array[:, :, 0], [end_points_array_yv, end_points_array_xv], order=1, mode='constant', cval=255)
+    end_points_array_yv += ndimage.map_coordinates(end_points_array[:, :, 1], [end_points_array_yv, end_points_array_xv], order=1, mode='constant', cval=255)
+    end_points_array_xv = np.remainder(end_points_array_xv, flow_width)
+    end_points_array_yv = np.remainder(end_points_array_yv, flow_height)   
+
+    # end_points_array_xv = sc.rotate_erp_array(end_points_array_xv,  rotation[0],  rotation[1])
+    # end_points_array_yv = sc.rotate_erp_array(end_points_array_yv,  rotation[0],  rotation[1])
     # # rotation the target points
     # phi_delta= rotation[1]
     # theta_delta = rotation[0]
@@ -262,18 +261,14 @@ def flow_accumulate_endpoint(optical_flow, rotation):
     # from envmap import EnvironmentMap
     # end_points_array_xv = EnvironmentMap(end_points_array_xv[:,:,np.newaxis], format_='latlong').rotate("DCM", rotation_matrix).data[:,:,0]
     # end_points_array_yv = EnvironmentMap(end_points_array_yv[:,:,np.newaxis], format_='latlong').rotate("DCM", rotation_matrix).data[:,:,0]
-
-    # erp -> spherical
-    end_points_sph = spherical_coordinates.erp2sph([end_points_array_xv, end_points_array_yv], erp_image_height=flow_height)
-
-    end_points_sph[0, :, :] += rotation[0] 
-    end_points_sph[1, :, :] += rotation[1]  # TODO correct
-
-    # spherical -> epr
-    end_points_erp_accu_x, end_points_erp_accu_y = spherical_coordinates.sph2erp(end_points_sph[0, :, :] , end_points_sph[1, :, :], flow_height)
+    # # erp -> spherical
+    # end_points_sph = sc.erp2sph([end_points_array_xv, end_points_array_yv], erp_image_height=flow_height)
+    # end_points_sph[0, :, :] += rotation[0]
+    # end_points_sph[1, :, :] += rotation[1]  # TODO correct
+    # # spherical -> epr
+    # end_points_erp_accu_x, end_points_erp_accu_y = sc.sph2erp(end_points_sph[0, :, :], end_points_sph[1, :, :], flow_height)
 
     # erp pixles location to flow
-    end_points_erp_accu_x -= src_points_array_xv
-    end_points_erp_accu_y -= src_points_array_yv
-
-    return np.stack((end_points_erp_accu_x, end_points_erp_accu_y), axis= -1)
+    end_points_array_xv -= src_points_array_xv
+    end_points_array_yv -= src_points_array_yv
+    return np.stack((end_points_array_xv, end_points_array_yv), axis=-1)

code/python/utility/spherical_coordinates.py
@@ -1,10 +1,14 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
+import spherical_coordinates as sc
+import flow_warp
+
 from logger import Logger
 
 log = Logger(__name__)
 log.logger.propagate = False
+
 
 def great_circle_distance(points_1, points_2, radius=1):
     """
@@ -142,7 +146,7 @@ def erp2sph(erp_points, erp_image_height=None, wrap_around=False):
     convert the point from erp image pixel location to spherical coordinate.
     The image center is spherical coordinate origin.
 
-    :param erp_points: the point location in ERP image (x,y), size is [2, :]
+    :param erp_points: the point location in ERP image x∊[0, width-1], y∊[0, height-1] , size is [2, :]
     :type erp_points: numpy
     :param erp_image_height: ERP image's height, defaults to None
     :type erp_image_height: int, optional
@@ -169,8 +173,10 @@ def erp2sph(erp_points, erp_image_height=None, wrap_around=False):
         erp_points_y = np.remainder(erp_points_y, height)
 
     # 1) point location to theta and phi
-    points_theta = (erp_points_x - (width - 1) * 0.5) * (2 * np.pi / width)
-    points_phi = -(erp_points_y - (height - 1) * 0.5) * (np.pi / height)
+    # points_theta = (erp_points_x - (width - 1) * 0.5) * (2 * np.pi / width)
+    # points_phi = -(erp_points_y - (height - 1) * 0.5) * (np.pi / height)
+    points_theta = erp_points_x * (2 * np.pi / width) + np.pi / width - np.pi
+    points_phi = -(erp_points_y * (np.pi / height) + np.pi / height * 0.5) + 0.5 * np.pi
     return np.stack((points_theta, points_phi))
 
 
@@ -189,12 +195,17 @@ def sph2erp(theta, phi, image_height, wrap_around=False):
     :return: the pixel location in the ERP image.
     :rtype: numpy
     """
-    x = (theta + np.pi) / (2.0 * np.pi) * (2 * image_height - 1)
-    y = -(phi - 0.5 * np.pi) / np.pi * (image_height - 1)
+    image_width = 2 * image_height
+    # x = (theta + np.pi) / (2.0 * np.pi) * (2 * image_height - 1)
+    # y = -(phi - 0.5 * np.pi) / np.pi * (image_height - 1)
+    # x = ((theta + np.pi) / (2.0 * np.pi / image_width)).astype(np.int)
+    # y = (-(phi - 0.5 * np.pi) / (np.pi / image_height)).astype(np.int)
+    x = (theta + np.pi - (np.pi / image_width)) / (2.0 * np.pi / image_width)
+    y = -(phi - 0.5 * np.pi + (np.pi / image_height * 0.5)) / (np.pi / image_height)
 
     # process the wrap around case
     if wrap_around:
-        x = np.remainder(x, image_height * 2)
+        x = np.remainder(x, image_width)
         y = np.remainder(y, image_height)
     return x, y
 
@@ -242,7 +253,25 @@ def sph2car(theta, phi, radius=1.0):
     return np.stack((x, y, z), axis=0)
 
 
-def rotate_array(data_array, rotate_theta, rotate_phi):
+def rotate_erp_array(erp_image, rotation_theta=None, rotation_phi=None, rotation_mat=None):
+    """ Rotate the ERP image with the theta and phi.
+
+    :param erp_image: The ERP image, [height, width, 3]
+    :type erp_image: numpy
+    :param rotation_theta: The source to target rotation's theta.
+    :type rotation_theta: float
+    :param rotation_phi: The source to target rotation's phi.
+    :type rotation_phi: float
+    """
+    # flow from tar to src
+    if rotation_mat is not None:
+        opticalflow, rotation_mat_ret = rotation2erp_motion_vector(erp_image.shape[0:2], rotation_matrix=rotation_mat)
+    else:
+        opticalflow, rotation_mat_ret = rotation2erp_motion_vector(erp_image.shape[0:2], -rotation_theta, -rotation_phi)
+    return flow_warp.warp_backward(erp_image, opticalflow), rotation_mat_ret    # the image backword warp
+
+
+def rotate_erp_array_skylib(data_array, rotate_theta, rotate_phi):
     """
     Rotate the image along the theta and phi.
 
@@ -265,46 +294,56 @@ def rotate_array(data_array, rotate_theta, rotate_phi):
     return data_array_rot
 
 
-def rotation_sph_coord(sph_theta, sph_phi, rotate_theta, rotate_phi):
-    """ rotate the sph spherical coordinate, and 
-    :param sph_xy: the spherical coordinate array, size is [2, points_number]
+def rotate_sph_coord(sph_theta, sph_phi, rotate_theta, rotate_phi):
+    """ Rotate the sph spherical coordinate with the rotation (theta, phi).
+
+    :param sph_theta: The spherical coordinate's theta array.
+    :type sph_theta: numpy
+    :param sph_phi: The spherical coordinate's phi array.
+    :type sph_phi: numpy
+    :param rotate_theta: The rotation along the theta
+    :type rotate_theta: float
+    :param rotate_phi: [description]
+    :type rotate_phi: float
+    :return: Target points theta and phi array.
+    :rtype: tuple
     """
     xyz = sph2car(sph_theta, sph_phi, radius=1.0)
     rotation_matrix = R.from_euler("xyz", [rotate_phi, rotate_theta, 0], degrees=False).as_matrix()
     xyz_rot = np.dot(rotation_matrix, xyz.reshape((3, -1)))
     array_xy_rot = car2sph(xyz_rot.T).T
-    return array_xy_rot[0,:] , array_xy_rot[1,:]
+    return array_xy_rot[0, :], array_xy_rot[1, :]
 
 
-def rotate_erp_motion_vector(array_size, rotate_theta, rotate_phi):
+def rotation2erp_motion_vector(array_size, rotate_theta=None, rotate_phi=None, rotation_matrix=None):
     """
-    Rotate the image's mesh grid.
+    Convert the spherical coordinate rotation to ERP coordinate motion flow.
+    With rotate the image's mesh grid.
 
-    :param data_array: the array size, [array_width, array_hight]
+    :param data_array: the array size, [array_hight, array_width]
     :type data_array: list
     :param rotate_theta: rotate along the longitude, radian
     :type rotate_theta: float
-    :param rotate_phi:  rotate along the latitude, radian
+    :param rotate_phi: rotate along the latitude, radian
     :type rotate_phi: float
     """
     # 1) generage spherical coordinate for each pixel
-    erp_x = np.linspace(0, array_size[0], array_size[0], endpoint=False)
-    erp_y = np.linspace(0, array_size[1], array_size[1], endpoint=False)
+    erp_x = np.linspace(0, array_size[1], array_size[1], endpoint=False)
+    erp_y = np.linspace(0, array_size[0], array_size[0], endpoint=False)
     erp_vx, erp_vy = np.meshgrid(erp_x, erp_y)
 
     # 1) spherical system to Cartesian system and rotate the points
-    sph_xy = erp2sph(np.stack((erp_vx, erp_vy)), erp_image_height=array_size[1], wrap_around=False)
+    sph_xy = erp2sph(np.stack((erp_vx, erp_vy)), erp_image_height=array_size[0], wrap_around=False)
+    # erp_x_rot, erp_y_rot = sph2erp(sph_xy[0, :], sph_xy[1, :], array_size[0], wrap_around=False)
     xyz = sph2car(sph_xy[0], sph_xy[1], radius=1.0)
-
-    rotation_matrix = R.from_euler("xyz", [rotate_phi, rotate_theta, 0], degrees=False).as_matrix()
-
+    if rotation_matrix is None:
+        rotation_matrix = R.from_euler("xyz", [rotate_phi, rotate_theta, 0], degrees=False).as_matrix()
     xyz_rot = np.dot(rotation_matrix, xyz.reshape((3, -1)))
-
     array_xy_rot = car2sph(xyz_rot.T).T
-    erp_x_rot, erp_y_rot = sph2erp(array_xy_rot[0, :], array_xy_rot[1, :], array_size[1], wrap_around=False)
+    erp_x_rot, erp_y_rot = sph2erp(array_xy_rot[0, :], array_xy_rot[1, :], array_size[0], wrap_around=False)
 
     # get motion vector
-    motion_vector_x = erp_x_rot.reshape((array_size[1], array_size[0])) - erp_vx
-    motion_vector_y = erp_y_rot.reshape((array_size[1], array_size[0])) - erp_vy
+    motion_vector_x = erp_x_rot.reshape((array_size[0], array_size[1])) - erp_vx
+    motion_vector_y = erp_y_rot.reshape((array_size[0], array_size[1])) - erp_vy
 
-    return np.stack((motion_vector_x, motion_vector_y))
+    return np.stack((motion_vector_x, motion_vector_y), -1), rotation_matrix